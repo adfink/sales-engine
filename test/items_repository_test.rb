5d21b3136e696266f6540bed886e625d4d7f6372
Andrew Fink <andrewfink8@gmail.com>
2015-04-21 17:54:14 -0600

    Items repo can now read and has the csv file contents stored in local var called contents

--- test/items_repository_test.rb
+++ test/items_repository_test.rb
@@ -8,4 +8,9 @@ class ItemsRepositoryTest < MiniTest::Test
     items = ItemsRepository.new
     assert items
   end
+
+  def test_that_it_can_load_the_csv_files
+    items = ItemsRepository.new
+    refute_equal "", items.get_info_from_csv
+  end
 end
