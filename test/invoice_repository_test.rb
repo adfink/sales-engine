6c7f32c7e274df71a56a704c96bec94cdc94d8dd
Max Tedford <max.tedford@gmail.com>
2015-04-21 14:26:05 -0600

    added initial tests and methods to recognize and read the csv data.

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -5,7 +5,17 @@ require './lib/invoice_repository'
 class InvoiceRepositoryTest < MiniTest::Test
 
   def test_that_it_exists
-    invoice = InvoiceRepository.new
-    assert invoice
+    invoice_repo = InvoiceRepository.new
+    assert invoice_repo
+  end
+
+  def test_that_it_can_find_the_file_to_read
+    invoice_repo = InvoiceRepository.new
+    assert invoice_repo.check_for_file
+  end
+
+  def test_that_it_can_output_the_contents_of_the_file
+    invoice_repo = InvoiceRepository.new
+    refute_equal "", invoice_repo.output_file_contents
   end
 end
