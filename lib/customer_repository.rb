bc7fd2cbb7a636246ffef17a4af7c7044e422e10
Max Tedford <max.tedford@gmail.com>
2015-04-21 17:34:16 -0600

    added method to recognize file and read/output its contents.

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,2 +1,11 @@
 class CustomerRepository
+
+  def locate_data_file
+    File.exist?"./data/customers.csv"
+  end
+
+  def output_file_contents
+    contents = File.read"./data/customers.csv"
+    puts contents
+  end
 end
