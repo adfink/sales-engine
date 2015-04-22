306b96ab719fd9213fddbe066cf73593aaa68dae
Max Tedford <max.tedford@gmail.com>
2015-04-22 15:38:10 -0600

    fixing some tests

--- test/items_repository_test.rb
+++ test/items_repository_test.rb
@@ -13,9 +13,4 @@ class ItemsRepositoryTest < MiniTest::Test
     items_repo = ItemsRepository.new
     refute_equal "", items_repo.output_file_contents
   end
-
-  def test_that_it_can_load_the_csv_files
-    items = ItemsRepository.new
-    refute_equal "", items.get_info_from_csv
-  end
 end
