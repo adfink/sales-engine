8cab1ab0a00db7e6e122ac2a9ea47846f99db531
Max Tedford <max.tedford@gmail.com>
2015-04-27 20:20:42 -0600

    making tests pass

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -68,6 +68,6 @@ class InvoiceTest < MiniTest::Test
     invoice_repo.create(customer: customer, merchant: merchant, status: "shipped", items: items)
 
     invoice = @engine.invoices_repository.invoices[-1]
-    assert_equal "", invoice.charge(credit_card_number: "4444333322221111", credit_card_expiration: "10/13", result:"success")
+    assert_equal 5596, invoice.charge(credit_card_number: "4444333322221111", credit_card_expiration: "10/13", result:"success").id
   end
 end
